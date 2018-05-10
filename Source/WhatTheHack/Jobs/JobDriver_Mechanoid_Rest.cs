6f7db2a6bb08d75496c1c3d2218e859f78057780
Roel <roelheirman@gmail.com>
2018-05-10 22:26:43 +0200

    mechs now stand up while resting

--- Source/WhatTheHack/Jobs/JobDriver_Mechanoid_Rest.cs
+++ Source/WhatTheHack/Jobs/JobDriver_Mechanoid_Rest.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Verse;
 using Verse.AI;
 
 namespace WhatTheHack.Jobs
@@ -16,9 +17,28 @@ namespace WhatTheHack.Jobs
 
         protected override IEnumerable<Toil> MakeNewToils()
         {
-            Toil toil = Toils_LayDown.LayDown(TargetIndex.A, true, false, false, false);
+            Toil layDownToil = Toils_LayDown.LayDown(TargetIndex.A, true, false, false, false);
+            layDownToil.defaultCompleteMode = ToilCompleteMode.Never;
+            layDownToil.AddPreTickAction(delegate
+            {
+                if (!pawn.health.hediffSet.HasNaturallyHealingInjury())
+                {
+                    Log.Message("ready for next toil!");
+                    ReadyForNextToil();
+                }
+            });
+            yield return layDownToil;
+            Toil toil = new Toil();
             toil.defaultCompleteMode = ToilCompleteMode.Never;
+            toil.initAction = delegate
+            {
+                Log.Message("Starting next toil");
+                pawn.CurJob.SetTarget(TargetIndex.C, new LocalTargetInfo(new IntVec3(pawn.Position.x, pawn.Position.y, pawn.Position.z - 1)));
+                this.rotateToFace = TargetIndex.C;
+
+            };
             yield return toil;
+
         }
     }
 }
