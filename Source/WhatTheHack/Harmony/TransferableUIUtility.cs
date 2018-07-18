4ac72b786f5dffe93ea0cdcdbc35b74d38b2544c
Roel <roelheirman@gmail.com>
2018-07-18 18:33:02 +0200

    added red color for low fuel in caravan dialog

--- Source/WhatTheHack/Harmony/TransferableUIUtility.cs
+++ Source/WhatTheHack/Harmony/TransferableUIUtility.cs
@@ -14,6 +14,13 @@ namespace WhatTheHack.Harmony
     {
         static void Prefix(ref List<TransferableUIUtility.ExtraInfo> info)
         {
+            Color color = Color.white;
+
+            if(Base.Instance.daysOfFuel < 1.0f)
+            {
+                color = Color.red;
+            }
+
             if(Base.Instance.daysOfFuelReason != "")
             {
                 info.Add(new TransferableUIUtility.ExtraInfo("WTH_ExtraInfoKey_DaysOfFuel".Translate(), Base.Instance.daysOfFuel.ToString("0.#"), Color.white, Base.Instance.daysOfFuelReason));
