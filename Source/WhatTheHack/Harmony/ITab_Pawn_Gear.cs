0d6499790175f0637e944747ec9ae93ad51387c2
Roel <roelheirman@gmail.com>
2018-08-02 12:05:08 +0200

    increased mass capacity for centipede + mechs cannot drop gear

---
+++ Source/WhatTheHack/Harmony/ITab_Pawn_Gear.cs
@@ -0,0 +1,27 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace WhatTheHack.Harmony
+{
+    [HarmonyPatch(typeof(ITab_Pawn_Gear), "InterfaceDrop")]
+    class ITab_Pawn_Gear_InterfaceDrop
+    {
+        static bool Prefix(ITab_Pawn_Gear __instance, Thing t)
+        {
+            Log.Message("calling InterfaceDrop");
+            Pawn pawn = Traverse.Create(__instance).Property("SelPawnForGear").GetValue<Pawn>();
+            if(pawn != null && pawn.IsHacked())
+            {
+                Log.Message("forbidding drop");
+                Messages.Message("WTH_Message_CannotDrop".Translate(), MessageTypeDefOf.RejectInput);
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Source/WhatTheHack/Harmony/MassUtility.cs
@@ -0,0 +1,26 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace WhatTheHack.Harmony
+{
+    [HarmonyPatch(typeof(MassUtility), "Capacity")]
+    class MassUtility_Capacity
+    {
+        static void Postfix(Pawn p, ref StringBuilder explanation, ref float __result)
+        {
+            if(p.def.HasModExtension<DefModExtension_PawnMassCapacity>())
+            {
+                __result += p.def.GetModExtension<DefModExtension_PawnMassCapacity>().bonusMassCapacity;
+                if(explanation != null)
+                {
+                    explanation.AppendLine("WTH_Explanation_BonusCapacity".Translate() + ": " + __result);
+                }
+            }
+        }
+    }
+}
