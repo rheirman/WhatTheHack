906a0b9af0e778171c85cac54da1854e4f527cc8
Roel <roelheirman@gmail.com>
2018-09-04 21:08:27 +0200

    repair upgrades also work while caravanning

--- Source/WhatTheHack/Harmony/Caravan.cs
+++ Source/WhatTheHack/Harmony/Caravan.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 using Verse;
+using WhatTheHack.Needs;
 
 namespace WhatTheHack.Harmony
 {
@@ -17,40 +18,94 @@ namespace WhatTheHack.Harmony
         {
             //Consume only fuel every interval of tickperday/fuelConsumptionRate. 
             float interval = GenDate.TicksPerDay / WTH_DefOf.WTH_PortableChargingPlatform.GetCompProperties<CompProperties_Refuelable>().fuelConsumptionRate;
-            if (__instance.IsHashIntervalTick(Mathf.RoundToInt(interval))){
-                int numMechanoids = 0;
-                int numPlatforms = 0;
-                foreach (Thing thing in __instance.AllThings)
+            if (__instance.IsHashIntervalTick(Mathf.RoundToInt(interval)))
+            {
+                ConsumeFuelIfNeeded(__instance);
+            }
+            if (__instance.IsHashIntervalTick(240))
+            {
+                RepairMechsIfNeeded(__instance);
+            }
+        }
+
+        private static void RepairMechsIfNeeded(Caravan __instance)
+        {
+            List<Thing> allParts = __instance.AllThings.Where((Thing t) => t.def == WTH_DefOf.WTH_MechanoidParts).ToList();
+            if (allParts.NullOrEmpty())
+            {
+                return;
+            }
+            Thing partItem = allParts.First();
+            if(partItem.stackCount < 5)
+            {
+                return;
+            }
+            List<Pawn> allHackedMechs = __instance.AllThings.Where((Thing t) => t is Pawn pawn && !pawn.Dead && pawn.IsHacked()).Cast<Pawn>().ToList();
+            if (allHackedMechs.NullOrEmpty())
+            {
+                return;
+            }
+            List<Pawn> allMechsNeedingRepairs = allHackedMechs.Where((Pawn p) => p.health.hediffSet.HasNaturallyHealingInjury() && !p.health.hediffSet.HasHediff(WTH_DefOf.WTH_Repairing) && (p.playerSettings == null || p.playerSettings.medCare != MedicalCareCategory.NoCare)).ToList();
+            if (allMechsNeedingRepairs.NullOrEmpty())
+            {
+                return;
+            }
+
+            foreach (Pawn pawn in allHackedMechs)
+            {
+                if(pawn.needs.TryGetNeed<Need_Power>() is Need_Power powerNeed && !powerNeed.OutOfPower)
                 {
-                    if (thing is Pawn && ((Pawn)thing).IsHacked())
+                    if (pawn.health != null && pawn.health.hediffSet.HasHediff(WTH_DefOf.WTH_RepairArm))
                     {
-                        numMechanoids++;
+                        allMechsNeedingRepairs.RandomElement().health.AddHediff(WTH_DefOf.WTH_Repairing);
+                        partItem.SplitOff(5);
+                        return;
                     }
-                    if (thing.def == ThingDefOf.MinifiedThing)
+                    if (pawn.health != null && pawn.health.hediffSet.HasHediff(WTH_DefOf.WTH_RepairModule) && pawn.health.hediffSet.HasNaturallyHealingInjury() && !pawn.health.hediffSet.HasHediff(WTH_DefOf.WTH_Repairing))
                     {
-                        if (thing.GetInnerIfMinified().def == WTH_DefOf.WTH_PortableChargingPlatform)
-                        {
-                            numPlatforms ++;
-                        }
-                    }
-                    if (thing.def == WTH_DefOf.WTH_PortableChargingPlatform)
-                    {
-                        numPlatforms += thing.stackCount;
+                        pawn.health.AddHediff(WTH_DefOf.WTH_Repairing);
+                        partItem.SplitOff(5);
+                        return;
                     }
                 }
-                foreach (Thing thing in __instance.AllThings)
+
+            }
+        }
+
+        private static void ConsumeFuelIfNeeded(Caravan __instance)
+        {
+            int numMechanoids = 0;
+            int numPlatforms = 0;
+            foreach (Thing thing in __instance.AllThings)
+            {
+                if (thing is Pawn && ((Pawn)thing).IsHacked())
                 {
-                    if (numMechanoids > 0 && numPlatforms > 0 && thing.def == ThingDefOf.Chemfuel && thing.stackCount > 0)
+                    numMechanoids++;
+                }
+                if (thing.def == ThingDefOf.MinifiedThing)
+                {
+                    if (thing.GetInnerIfMinified().def == WTH_DefOf.WTH_PortableChargingPlatform)
                     {
-                        int fuelConsumedThisInterval = Math.Min(numMechanoids, numPlatforms);
-                        thing.SplitOff(fuelConsumedThisInterval).Destroy(DestroyMode.Vanish);
-                        if(thing.stackCount == 0) {
-                            Messages.Message("WTH_Message_CaravanOutOfFuel".Translate(new object[]{__instance.LabelCap}), __instance, MessageTypeDefOf.ThreatBig, true);
-                        }
+                        numPlatforms++;
+                    }
+                }
+                if (thing.def == WTH_DefOf.WTH_PortableChargingPlatform)
+                {
+                    numPlatforms += thing.stackCount;
+                }
+            }
+            foreach (Thing thing in __instance.AllThings)
+            {
+                if (numMechanoids > 0 && numPlatforms > 0 && thing.def == ThingDefOf.Chemfuel && thing.stackCount > 0)
+                {
+                    int fuelConsumedThisInterval = Math.Min(numMechanoids, numPlatforms);
+                    thing.SplitOff(fuelConsumedThisInterval).Destroy(DestroyMode.Vanish);
+                    if (thing.stackCount == 0)
+                    {
+                        Messages.Message("WTH_Message_CaravanOutOfFuel".Translate(new object[] { __instance.LabelCap }), __instance, MessageTypeDefOf.ThreatBig, true);
                     }
                 }
             }
-           
         }
     }
 }
