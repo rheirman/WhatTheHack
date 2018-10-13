e376e89c903e0ac99d6cee1e555ae61cd810dba1
Roel <roelheirman@gmail.com>
2018-10-13 20:57:21 +0200

    almost functioning mech beacon

---
+++ Source/WhatTheHack/Harmony/IncidentWorker_RaidEnemy.cs
@@ -0,0 +1,26 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace WhatTheHack.Harmony
+{
+    [HarmonyPatch(typeof(IncidentWorker_RaidEnemy), "TryResolveRaidFaction")]
+    class IncidentWorker_RaidEnemy_TryResolveRaidFaction
+    {
+        static bool Prefix(ref IncidentParms parms, ref bool __result)
+        {
+            if(parms.target != null && parms.target.IncidentTargetTags().Contains(IncidentTargetTagDefOf.Map_RaidBeacon))
+            {
+                Log.Message("TryResolveRaidFaction patch called");
+                __result = true;
+                parms.faction = Faction.OfMechanoids;
+                return false;
+            }
+            return true;
+        }
+    }
+}
