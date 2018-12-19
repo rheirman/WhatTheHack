cac2d991f3fd70f6e4f4055969a77c76216df41b
Roel <roelheirman@gmail.com>
2018-12-19 14:17:20 +0100

    fixed NRE when looking at info tab of mechanoids

---
+++ Source/WhatTheHack/Harmony/CompProperties_Refuelable.cs
@@ -0,0 +1,24 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace WhatTheHack.Harmony
+{
+    //We added comprefuelable to mechanoids. Vanilla code assumes it to be used for buildings only, therefore this extra check to prevent errors. 
+    [HarmonyPatch(typeof(CompProperties_Refuelable), "SpecialDisplayStats")]
+    class CompProperties_Refuelable_SpecialDisplayStats
+    {
+        static bool Prefix(StatRequest req)
+        {
+            if(((ThingDef)req.Def).building == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
