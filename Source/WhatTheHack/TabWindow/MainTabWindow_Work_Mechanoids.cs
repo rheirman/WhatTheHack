9b80872a74640a13cf4c1d701d1ac6f36b0604b2
Roel <roelheirman@gmail.com>
2019-02-19 17:47:50 +0100

    Revert to cb762791fcb7079be84772ec48f1b7ccae06d65c

---
+++ Source/WhatTheHack/TabWindow/MainTabWindow_Work_Mechanoids.cs
@@ -0,0 +1,31 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace WhatTheHack.TabWindow
+{
+    class MainTabWindow_Work_Mechanoids : MainTabWindow_Work
+    {
+        protected override IEnumerable<Pawn> Pawns
+        {
+            get
+            {
+                return from p in Find.CurrentMap.mapPawns.PawnsInFaction(Faction.OfPlayer)
+                       where p.IsHacked()
+                       select p;
+            }
+        }
+        /*
+        protected override PawnTableDef PawnTableDef
+        {
+            get
+            {
+                return WTH_DefOf.WTH_PawnTableDef_Mechanoids;
+            }
+        }
+        */
+    }
+}
