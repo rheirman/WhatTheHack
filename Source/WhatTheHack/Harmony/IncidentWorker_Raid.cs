4b0a553e5f8935a82700f771e403a755dec50977
Roel <roelheirman@gmail.com>
2019-02-22 22:20:02 +0100

    added modules for enemy hacked mechs - needs balancing

--- Source/WhatTheHack/Harmony/IncidentWorker_Raid.cs
+++ Source/WhatTheHack/Harmony/IncidentWorker_Raid.cs
@@ -6,12 +6,14 @@ using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
 using Verse;
+using WhatTheHack.Comps;
 using WhatTheHack.Needs;
 
 namespace WhatTheHack.Harmony
 {
     //Spawn hacked mechnoids in enemy raids
     [HarmonyPatch(typeof(IncidentWorker_Raid), "TryExecuteWorker")]
+    [HarmonyPriority(Priority.High)]
     public static class IncidentWorker_Raid_TryExecuteWorker
     {
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
@@ -20,11 +22,11 @@ namespace WhatTheHack.Harmony
             for (var i = 0; i < instructionsList.Count; i++)
             {
                 CodeInstruction instruction = instructionsList[i];
-
+                            
                 //Replace Arrive method by SpawnHackedMechanoids (which also calls Arrive). This avoids the need of ldgarg calls, which seem to change after literally every update of Rimworld.
                 if (instruction.operand == AccessTools.Method(typeof(PawnsArrivalModeWorker), "Arrive"))
                 {
-                    yield return new CodeInstruction(OpCodes.Call, typeof(IncidentWorker_Raid_TryExecuteWorker).GetMethod("SpawnHackedMechanoids"));//don't execute this, execute it in MountAnimals
+                    yield return new CodeInstruction(OpCodes.Call, typeof(IncidentWorker_Raid_TryExecuteWorker).GetMethod("SpawnHackedMechanoids"));
                     continue;
                 }
                 else
@@ -59,7 +61,6 @@ namespace WhatTheHack.Harmony
             float maxMechPoints = parms.points * ((float)rand.Next(minHackedMechPoints, Base.maxHackedMechPoints)) / 100; //TODO: no magic numbers
             float cumulativePoints = 0;
             Map map = parms.target as Map;
-
             while (cumulativePoints < maxMechPoints)
             {
                 PawnKindDef pawnKindDef = null;
@@ -76,6 +77,8 @@ namespace WhatTheHack.Harmony
                 }
                 if (pawnKindDef != null)
                 {
+
+
                     Pawn mechanoid = PawnGenerator.GeneratePawn(pawnKindDef, parms.faction);
                     IntVec3 loc = CellFinder.RandomClosewalkCellNear(parms.spawnCenter, map, 8, null);
                     GenSpawn.Spawn(mechanoid, loc, map, parms.spawnRotation);
@@ -85,6 +88,8 @@ namespace WhatTheHack.Harmony
                     powerNeed.CurLevel = powerNeed.MaxLevel;
                     pawns.Add(mechanoid);
                     cumulativePoints += pawnKindDef.combatPower;
+                    AddModules(mechanoid);
+
                 }
                 else
                 {
@@ -100,6 +105,41 @@ namespace WhatTheHack.Harmony
                 }
             }
             return pawns;
+        }
+
+        private static void AddModules(Pawn mechanoid)
+        {
+            List<HediffDef> modules = new List<HediffDef>();
+            modules.Add(WTH_DefOf.WTH_SpeedModule);
+            modules.Add(WTH_DefOf.WTH_TurretModule);
+            modules.Add(WTH_DefOf.WTH_VanometricModule);
+
+            int i = 0;
+            int count = modules.Count;
+            while (i < count)
+            {
+                if (Rand.Chance(0))
+                {//Chance that the mod is NOT used
+                    modules.RemoveAt(i);
+                }
+                i++;
+            }
+            foreach (HediffDef hediff in modules)
+            {
+                if (hediff == WTH_DefOf.WTH_TurretModule)
+                {
+                    if (mechanoid.BodySize < 2.0f)
+                    {
+                        continue;
+                    }
+                    mechanoid.health.AddHediff(WTH_DefOf.WTH_MountedTurret);
+                    ThingDef turretDef = DefDatabase<ThingDef>.AllDefs.Where((ThingDef td) => td.HasComp(typeof(CompMountable))).RandomElement();
+                    Thing thing = ThingMaker.MakeThing(turretDef, ThingDefOf.Steel);
+                    CompMountable comp = thing.GetInnerIfMinified().TryGetComp<CompMountable>();
+                    comp.MountToPawn(mechanoid);
+                }
+                mechanoid.health.AddHediff(hediff);
+            }
         }
         //do nothing
     }
