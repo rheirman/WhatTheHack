236d253a1a2bb13200bd37d1c637e2c047669707
Roel <roelheirman@gmail.com>
2019-03-09 14:49:28 +0100

    added labels for mechs + platforms

--- Source/WhatTheHack/Buildings/Building_BaseMechanoidPlatform.cs
+++ Source/WhatTheHack/Buildings/Building_BaseMechanoidPlatform.cs
@@ -53,6 +53,5 @@ namespace WhatTheHack.Buildings
                 hotKey = KeyBindingDefOf.Misc3
             };
         }
-
     }
 }
---
+++ Source/WhatTheHack/Harmony/PawnUIOverlay.cs
@@ -0,0 +1,25 @@
+﻿using Harmony;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace WhatTheHack.Harmony
+{
+    [HarmonyPatch(typeof(PawnUIOverlay), "DrawPawnGUIOverlay")]
+    class PawnUIOverlay_DrawPawnGUIOverlay
+    {
+        static void Postfix(PawnUIOverlay __instance)
+        {
+            Pawn pawn = Traverse.Create(__instance).Field("pawn").GetValue<Pawn>();
+            if(pawn.IsHacked() && pawn.Faction == Faction.OfPlayer && pawn.Name != null)
+            {
+                Vector2 pos = GenMapUI.LabelDrawPosFor(pawn, -0.6f);
+                GenMapUI.DrawPawnLabel(pawn, pos, 1f, 9999f, null, GameFont.Tiny, true, true);
+            }
+        }
+    }
+}
