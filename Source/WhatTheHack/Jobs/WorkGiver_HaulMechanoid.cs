bb40055935c2aac8413dfd1fb9fb7698e05744e5
Roel <roelheirman@gmail.com>
2019-05-09 20:54:06 +0200

    fixed conflict with Haul Explicitly

--- Source/WhatTheHack/Jobs/WorkGiver_HaulMechanoid.cs
+++ Source/WhatTheHack/Jobs/WorkGiver_HaulMechanoid.cs
@@ -1,4 +1,5 @@
-﻿using RimWorld;
+﻿using Harmony;
+using RimWorld;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,7 +35,7 @@ namespace WhatTheHack.Jobs
             {
                 return null;
             }
-            if(mech != null && HaulAIUtility.PawnCanAutomaticallyHaulFast(pawn, t, forced))
+            if(mech != null && PawnCanAutomaticallyHaulFast(pawn, t, forced))
             {
                 Building_HackingTable closestAvailableTable = Utilities.GetAvailableHackingTable(pawn, mech);
 
@@ -44,6 +45,36 @@ namespace WhatTheHack.Jobs
                 }
             }
             return result;
+        }
+
+        //Copied from vanilla to prevent it from being broken by other mods. HaulExplicitly for instance would break this otherwise. 
+        private static bool PawnCanAutomaticallyHaulFast(Pawn p, Thing t, bool forced)
+        {
+            UnfinishedThing unfinishedThing = t as UnfinishedThing;
+            if (unfinishedThing != null && unfinishedThing.BoundBill != null)
+            {
+                return false;
+            }
+            if (!p.CanReach(t, PathEndMode.ClosestTouch, p.NormalMaxDanger(), false, TraverseMode.ByPawn))
+            {
+                return false;
+            }
+            LocalTargetInfo target = t;
+            if (!p.CanReserve(target, 1, -1, null, forced))
+            {
+                return false;
+            }
+            if (t.def.IsNutritionGivingIngestible && t.def.ingestible.HumanEdible && !t.IsSociallyProper(p, false, true))
+            {
+                JobFailReason.Is(Traverse.Create(typeof(HaulAIUtility)).Field("ReservedForPrisonersTrans").GetValue<string>(), null);
+                return false;
+            }
+            if (t.IsBurning())
+            {
+                JobFailReason.Is(Traverse.Create(typeof(HaulAIUtility)).Field("BurningLowerTrans").GetValue<string>(), null);
+                return false;
+            }
+            return true;
         }
     }
 }
