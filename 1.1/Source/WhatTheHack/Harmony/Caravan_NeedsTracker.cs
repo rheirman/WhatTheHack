9161db891d7ee48b6fe51409c0a82b9ba84f7a2b
rheirman <roelheirman@gmail.com>
2020-08-16 18:38:24 +0200

    also applied needs fix in 1.1 version

--- 1.1/Source/WhatTheHack/Harmony/Caravan_NeedsTracker.cs
+++ 1.1/Source/WhatTheHack/Harmony/Caravan_NeedsTracker.cs
@@ -4,23 +4,40 @@ using RimWorld.Planet;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using Verse;
 
 namespace WhatTheHack.Harmony
 {
-    //Can wear belts d
+    // Patch that prevents a null reference error when a pawn doesn't have psychic entropy (which is always the case for mechs). 
     [HarmonyPatch(typeof(Caravan_NeedsTracker), "TrySatisfyPawnNeeds")]
-    class Caravan_NeedsTracker_TrySatisfyPawnNeeds
+    static class Caravan_NeedsTracker_TrySatisfyPawnNeeds
     {
-        static void Postfix(Pawn p, ThingDef apparel, ref bool __result)
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilg)
         {
-            if(!__result && Utilities.IsBelt(apparel.apparel) && p.health != null && p.health.hediffSet.HasHediff(WTH_DefOf.WTH_BeltModule)) //TODO: make more general
+            Label label = ilg.DefineLabel();
+
+            var instructionsList = new List<CodeInstruction>(instructions);
+            for (var i = 0; i < instructionsList.Count; i++)
             {
-                __result = true;
+                CodeInstruction instruction = instructionsList[i];
+                yield return instruction;
+
+                if (instruction.operand as FieldInfo == AccessTools.Field(typeof(Pawn), "psychicEntropy"))
+                {
+                    yield return new CodeInstruction(OpCodes.Dup);
+                    yield return new CodeInstruction(OpCodes.Isinst, typeof(Pawn_PsychicEntropyTracker));
+                    instructionsList[i + 1].labels.Add(label);
+                    yield return new CodeInstruction(OpCodes.Brtrue, label);
+                    yield return new CodeInstruction(OpCodes.Pop);
+                    yield return new CodeInstruction(OpCodes.Ret);
+                }
+
             }
         }
     }
 
-   
+
 }
