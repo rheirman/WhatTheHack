94bd63d29a115123b802d6b9e0720090a0f3acab
Mlie <emipa606@users.noreply.github.com>
2023-01-19 12:48:51 +0100

    Restored the old csproj files and splitted up some class-files

--- Source/WhatTheHack/Harmony/Building_Bed_get_DrawColor.cs
+++ Source/WhatTheHack/Harmony/Building_Bed_get_DrawColor.cs
@@ -1,7 +1,6 @@
 ﻿using HarmonyLib;
 using RimWorld;
 using UnityEngine;
-using Verse;
 using WhatTheHack.Buildings;
 
 namespace WhatTheHack.Harmony;
@@ -15,31 +14,5 @@ internal static class Building_Bed_get_DrawColor
         {
             __result = new Color(1f, 1f, 1f);
         }
-    }
-}
-
-[HarmonyPatch(typeof(JobGiver_GetEnergy), "ShouldAutoRecharge")]
-internal static class JobGiver_GetEnergy_ShouldAutoRecharge
-{
-    private static void Postfix(ref bool __result, Pawn pawn)
-    {
-        if (!__result)
-        {
-            return;
-        }
-
-        if (pawn.IsHacked())
-        {
-            __result = false;
-        }
-    }
-}
-
-[HarmonyPatch(typeof(Need_MechEnergy), "NeedInterval")]
-internal static class Need_MechEnergy_NeedInterval
-{
-    private static bool Prefix(Need_MechEnergy __instance)
-    {
-        return !__instance.pawn.IsHacked();
     }
 }
---
+++ Source/WhatTheHack/Harmony/JobGiver_GetEnergy_ShouldAutoRecharge.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace WhatTheHack.Harmony;
+
+[HarmonyPatch(typeof(JobGiver_GetEnergy), "ShouldAutoRecharge")]
+internal static class JobGiver_GetEnergy_ShouldAutoRecharge
+{
+    private static void Postfix(ref bool __result, Pawn pawn)
+    {
+        if (!__result)
+        {
+            return;
+        }
+
+        if (pawn.IsHacked())
+        {
+            __result = false;
+        }
+    }
+}
